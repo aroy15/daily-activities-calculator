cedb67c40dc17453c7c631cd73e92e6e7255a644
aroy15 <anjonroy57@gmail.com>
2022-09-29 13:22:33 +0600

    site favicon change, site's main title design update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPersonDigging } from '@fortawesome/free-solid-svg-icons';
+import './Header.css';
 
 const Header = () => {
     return (
         <header>
-            <h1>Daily Activities</h1>
+            <h1 className='site-name'><FontAwesomeIcon icon={faPersonDigging} />  Daily Activities</h1>
             <h2>Select today’s activities</h2>
         </header>
     );
