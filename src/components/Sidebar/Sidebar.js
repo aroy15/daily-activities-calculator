23dbd774cec298eb58b5510a6e994bed49fc7471
aroy15 <anjonroy57@gmail.com>
2022-09-29 14:25:10 +0600

    Add a break functionality added

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Sidebar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import profileImage from '../../image/anjon-profile.jpg';
 
-const Sidebar = ({times}) => {
+const Sidebar = ({times}) => {    
+    const [addTime, setAddTime] = useState(0);
+    const breakTimes = [20,10,30,40,50];
+    
+    const breakTimeFunc = breakTime =>{
+        setAddTime(breakTime);
+    }
     return (
         <div className='sidebar-inner'>
             <div className="profile d-flex align-i-center gap-20">
@@ -31,17 +37,20 @@ const Sidebar = ({times}) => {
             <div className="break-info">
                 <h3>Add A Break</h3>
                 <div className="break-inner d-flex align-i-center justify-between">
-                    <div className="break-item">20s</div>
+                    {
+                        breakTimes.map((breakTime, index) => <div key={`id${index}`} onClick={()=>breakTimeFunc(breakTime)} className="break-item">{breakTime}s</div>)
+                    }
+                    {/* <div className="break-item">20s</div>
                     <div className="break-item">10s</div>
                     <div className="break-item">30s</div>
                     <div className="break-item">40s</div>
-                    <div className="break-item">50s</div>
+                    <div className="break-item">50s</div> */}
                 </div>
             </div>
             <div className="activity-details">
                 <h3>Activity Details</h3>
                 <div className="time-exr-br d-flex gap-20 align-i-center justify-between">Activity time <span className="times">{times} second</span></div>
-                <div className="time-exr-br d-flex gap-20 align-i-center justify-between">Break time <span className="times">15 second</span></div>
+                <div className="time-exr-br d-flex gap-20 align-i-center justify-between">Break time <span className="times">{addTime} second</span></div>
             </div>
             <button className='common-btn'>Activity Completed</button>
         </div>
