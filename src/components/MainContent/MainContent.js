48aa238600a980751177b83dc270bfbc6231deab
aroy15 <anjonroy57@gmail.com>
2022-09-29 11:12:54 +0600

    MainContent.css file creation, move app's css to MainContent.css file

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../Header/Header';
+import './MainContent.css'
 
 const MainContent = () => {
     const [activities, setActivities] = useState([]);
