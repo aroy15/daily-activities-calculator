72bc016df6cdb1e03533f28020e3233446754b73
aroy15 <anjonroy57@gmail.com>
2022-09-29 12:23:48 +0600

    react font awesome installation done, profile design done

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './Sidebar.css'
+import './Sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import profileImage from '../../image/anjon-profile.jpg';
 
 const Sidebar = () => {
     return (
         <div className='sidebar-inner'>
-            sidebar found
+            <div className="profile d-flex align-i-center gap-20">
+                <img src={profileImage} alt="" />
+                <div>
+                    <h3 className='name'>Anjon Roy</h3>
+                    <p className='address'><FontAwesomeIcon icon={faLocationDot} /> Chattogram, Bangladesh</p>
+                </div>
+            </div>
         </div>
     );
 };
